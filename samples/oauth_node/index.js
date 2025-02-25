4bbce9dbd082ef04a453673cff0d7561d5b3e2a5
Steve Meyer <stevenmeyer142@gmail.copm>
2025-02-25 12:15:31 -0500

    login successful

--- samples/oauth_node/index.js
+++ samples/oauth_node/index.js
@@ -2,54 +2,68 @@ require('dotenv').config();
 const axios = require('axios');
 const express = require('express');
 const session = require('express-session');
-const { Issuer, Strategy } = require('openid-client');
+//const { Issuer, Strategy } = require('openid-client');
 const passport = require('passport');
+const OAuth2Strategy = require('passport-oauth2');
 const https = require('https');
 const sqlite3 = require('sqlite3').verbose();
 const env = process.env.ENVIRONMENT;
-const ROOT_URL = `https://${env}-api.va.gov/oauth2/.well-known/openid-configuration`;
+//const ROOT_URL = `https://${env}-api.va.gov/oauth2/.well-known/openid-configuration`;
 const client_id = process.env.CLIENT_ID;
 const client_secret = process.env.CLIENT_SECRET;
 var bodyParser = require('body-parser');
+const { log } = require('console');
 const redirect_uri = 'http://localhost:8081/auth/cb';
+const authenticationURL = `https://${env}-api.va.gov/oauth2/claims/v1/authorization`;
+const requestTokenURL = `https://${env}-api.va.gov/oauth2/claims/v1/token`;
+const accessTokenURL = `https://${env}-api.va.gov/oauth2/claims/v1/token`;
+const nonce = "14343103be036d10b974c40b6eb7c6553f0b91c0f766f1e3f7358d76c377bb8d";
+const scope="profile openid offline_access claim.read claim.write";
+// const scope = "profile openid offline_access launch/patient patient/AllergyIntolerance.read patient/Appointment.read patient/Binary.read patient/Condition.read patient/Device.read patient/DeviceRequest.read patient/DiagnosticReport.read patient/DocumentReference.read patient/Encounter.read patient/Immunization.read patient/Location.read patient/Medication.read patient/MedicationOrder.read patient/MedicationRequest.read patient/MedicationStatement.read patient/Observation.read patient/Organization.read patient/Patient.read patient/Practitioner.read patient/PractitionerRole.read patient/Procedure.read";
 
-const createClient = async () => {
-  Issuer.defaultHttpOptions = { timeout: 2500 };
-  return Issuer.discover(ROOT_URL).then(issuer => {
-    return new issuer.Client({
-      client_id,
-      client_secret,
-      redirect_uris: [
-        'http://localhost:8081/auth/cb'
-      ],
-    });
-  });
-}
+// const createClient = async () => {
+//   Issuer.defaultHttpOptions = { timeout: 2500 };
+//   return Issuer.discover(ROOT_URL).then(issuer => {
+//     return new issuer.Client({
+//       client_id,
+//       client_secret,
+//       redirect_uris: [
+//         'http://localhost:8081/auth/cb'
+//       ],
+//     });
+//   });
+// }
 
-const configurePassport = (client) => {
+const configurePassport = () => {
   passport.serializeUser((user, done) => {
+    console.log('serializeUser', user);
     done(null, user);
   });
 
   passport.deserializeUser((user, done) => {
+    console.log('deserializeUser', user);
     done(null, user);
   });
 
-  passport.use('oidc', new Strategy(
-    {
-      client,
-      params: {
-        scope: 'openid profile veteran_status.read claim.read',
-      },
-    }, (tokenset, userinfo, done) => {
-      done(null, { userinfo, tokenset });
-    }
-  ));
+  passport.use("oauth2", new OAuth2Strategy({
+    authorizationURL: authenticationURL,
+    tokenURL: requestTokenURL,
+    clientID: client_id,
+    clientSecret: client_secret,
+    scope: scope,
+    state: true,
+    callbackURL: redirect_uri
+
+  },
+  function(accessToken, refreshToken, profile, cb) {
+    cb(null, { accessToken, refreshToken, profile });
+  }
+));
 }
 
 const userDetails = async (req, res, next) => {
-  if (req.session && req.session.passport && req.session.passport.user) {
-    res.send(req.session.passport.user);
+  if (loggedIn) {
+    res.send(req.session.user);
     next();
   } else {
     res.redirect('/auth'); // Redirect the user to login if they are not
@@ -58,11 +72,13 @@ const userDetails = async (req, res, next) => {
 }
 
 const verifyVeteranStatus = async (req, res, next) => {
-  if (req.session && req.session.passport && req.session.passport.user) {
+  if (loggedIn(req)) {
+    const access_token = req.session.user.accessToken;
+    const has_token = access_token !== undefined;
     const veteranStatus = await new Promise((resolve, reject) => {
       https.get(
         `https://${env}-api.va.gov/services/veteran_verification/v2/status`,
-        { headers: {'Authorization': `Bearer ${req.session.passport.user.tokenset.access_token}`} },
+        { headers: {'Authorization': `Bearer ${access_token}`} },
         (res) => {
           let rawData = '';
           if (res.statusCode !== 200) {
@@ -81,7 +97,7 @@ const verifyVeteranStatus = async (req, res, next) => {
         }
       ).on('error', reject);
     });
-    res.render('status', { tokenset: req.session.passport.user.tokenset, veteranStatus: veteranStatus, user: req.session.passport.user });
+    res.render('status', { has_token: has_token, veteranStatus: veteranStatus, user: req.session.user });
     next();
   } else {
     res.redirect('/auth'); // Redirect the user to login if they are not
@@ -97,25 +113,34 @@ const wrapAuth = async (req, res, next) => {
     return next(req.query.error_description);
   }
   
-  console.log('wrapAuth response ', req.query);
+  //console.log('wrapAuth response ', req.query);
   const code = req.query.code;
   
+  passport.authenticate('oauth2', function(err, user, info, status) {
+    if (err) { return next(err) }
+    if (!user) { return res.redirect('/auth') }
+    req.session.user = user;
+    res.redirect('/home');
+  })(req, res, next);
+
+
+
   
- // passport.authenticate("oidc", { successRedirect: "/home", failureRedirect: "/"})(req, res, next);
- const data = new URLSearchParams();
-data.append('grant_type', 'authorization_code');
-data.append('code', code);
-data.append('redirect_uri', redirect_uri);
-const url = `https://${env}-api.va.gov/oauth2/claims/v1/token`;
-const authorization = 'Basic ' + Buffer.from(client_id + ':' + client_secret).toString('base64');
-const request = new Request(url, {
-  method: 'POST',
-  headers: {
-    'Content-Type': 'application/x-www-form-urlencoded',
-    'Authorization': authorization
-  },
-  body: data
-});
+// passport.authenticate("oidc", { successRedirect: "/home", failureRedirect: "/"})(req, res, next);
+//  const data = new URLSearchParams();
+// data.append('grant_type', 'authorization_code');
+// data.append('code', code);
+// data.append('redirect_uri', redirect_uri);
+// const url = `https://${env}-api.va.gov/oauth2/claims/v1/token`;
+// const authorization = 'Basic ' + Buffer.from(client_id + ':' + client_secret).toString('base64');
+// const request = new Request(url, {
+//   method: 'POST',
+//   headers: {
+//     'Content-Type': 'application/x-www-form-urlencoded',
+//     'Authorization': authorization
+//   },
+//   body: data
+// });
 
 
 // console.log('\nRequest url', request.url, "\n ");
@@ -127,18 +152,21 @@ const request = new Request(url, {
 
 
 // console.log('\nRequest ', request, "\n ");
-fetch(request)
-	.then(response => response.json())
-	.then(data => console.log(data))
-	.catch(error => console.error(error));
+// fetch(request)
+// 	.then(response => response.json())
+// 	.then(data => console.log(data))
+// 	.catch(error => console.error(error));
 
 };
 
 const loggedIn = (req) => {
-  return req.session && req.session.passport && req.session.passport.user;
+  console.log('req.session', req.session);
+  console.log('req.session.user', req.session.user);
+
+   return req.session && req.session.user;
 }
 
-const startApp = (client) => {
+const startApp = () => {
   const app = express();
   const port = 8081;
   const secret = 'My Super Secret Secret'
@@ -158,36 +186,30 @@ const startApp = (client) => {
   app.use(bodyParser.urlencoded({ extended: true }));
 
   app.get('/', (req, res) => {
-    var user = {};
+    const has_token = req.session.user?.accessToken !== undefined;
     
-    nonce = "14343103be036d10b974c40b6eb7c6553f0b91c0f766f1e3f7358d76c377bb8d";
-    const scope="profile openid offline_access claim.read claim.write";
-   // const scope = "profile openid offline_access launch/patient patient/AllergyIntolerance.read patient/Appointment.read patient/Binary.read patient/Condition.read patient/Device.read patient/DeviceRequest.read patient/DiagnosticReport.read patient/DocumentReference.read patient/Encounter.read patient/Immunization.read patient/Location.read patient/Medication.read patient/MedicationOrder.read patient/MedicationRequest.read patient/MedicationStatement.read patient/Observation.read patient/Organization.read patient/Patient.read patient/Practitioner.read patient/PractitionerRole.read patient/Procedure.read";
     const url = `https://${env}-api.va.gov/oauth2/claims/v1/authorization?client_id=${client_id}&nonce=${nonce}&redirect_uri=${redirect_uri}&response_type=code&scope=${scope}&state=1589217940`;
   
-    console.log("\nAuthorization url ", url, "\n");
+    //console.log("\nAuthorization url ", url, "\n");
     if (loggedIn(req)) {
-      user = req.session.passport.user;
-      req.session.user = req.session.passport.user;
-      req.session.tokenset = user.tokenset;
-      res.render('index', { tokenset: user.tokenset, autherizeLink : url } )
+       res.render('index', { has_token: has_token, autherizeLink : url } )
     } else {
-      res.render('index', { tokenset: {}, autherizeLink : url   } )
+      res.render('index', { has_token: has_token, autherizeLink : url   } )
     }
   });
 
   app.get('/status', verifyVeteranStatus);
   app.get('/userdetails', userDetails);
   app.get('/coming_soon', (req, res) => {
-    res.render('coming_soon', { tokenset: {},} )
+    res.render('coming_soon', { has_token: {},} )
   })
 
   app.get('/home', (req, res) => {
-    console.log('home');
+    console.log('home req.session.user', req.session.user);
     if (loggedIn(req)) {
       const users = [];
       const sql = `SELECT id, first_name, last_name, social_security_number, birth_date FROM veterans`;
-      const tokenset = req.session.passport.user.tokenset;
+      const has_token = req.session.user?.accessToken !== undefined;
       db.all(sql, [], (err, rows) => {
         if (err) {
           throw err;
@@ -195,7 +217,7 @@ const startApp = (client) => {
         rows.forEach((row) => {
           users.push(row)
         });
-        res.render('home', { tokenset, users });
+        res.render('home', { has_token, users });
       });
 
     } else {
@@ -205,14 +227,15 @@ const startApp = (client) => {
 
   app.get('/claims', (req, res) => {
     if (loggedIn(req)) {
-      const tokenset = req.session.passport.user.tokenset;
+      const access_token = req.session.user.accessToken;
+      const has_token = access_token !== undefined;
       axios.get(`https://${env}-api.va.gov/services/claims/v1/claims`, {
         headers: {
-          Authorization: `Bearer ${tokenset.access_token}`
+          Authorization: `Bearer ${access_token}`
         }
       })
       .then(response => {
-        res.render('claims', { claims: response.data.data, tokenset: tokenset });
+        res.render('claims', { claims: response.data.data, has_token: has_token });
       })
       .catch(error => {
         console.log(error)
@@ -223,18 +246,19 @@ const startApp = (client) => {
   });
 
   app.get('/claims/for/:id', (req, res) => {
-    if (req.session && req.session.passport && req.session.passport.user) {
+    if (loggedIn(req)) {
       const id = req.params.id;
       const users = [];
       const sql = `SELECT id, first_name, last_name, social_security_number, birth_date FROM veterans where id = ?`;
-      const tokenset = req.session.passport.user.tokenset;
+      const access_token = req.session.user.accessToken;
+      const has_token = access_token !== undefined;
       db.get(sql, [id], (err, row) => {
         if (err) {
           throw err;
         }
         axios.get(`https://${env}-api.va.gov/services/claims/v1/claims`, {
           headers: {
-            Authorization: `Bearer ${tokenset.access_token}`,
+            Authorization: `Bearer ${access_token}`,
             'X-VA-First-Name': row.first_name,
             'X-VA-Last-Name': row.last_name,
             'X-VA-Birth-Date': row.birth_date,
@@ -242,7 +266,7 @@ const startApp = (client) => {
           }
         })
         .then(response => {
-          res.render('claims', { user: `${row.first_name} ${row.last_name}`, claims: response.data.data, tokenset: tokenset });
+          res.render('claims', { user: `${row.first_name} ${row.last_name}`, claims: response.data.data, has_token: has_token });
         })
         .catch(error => {
           console.log(error)
@@ -273,7 +297,7 @@ const startApp = (client) => {
     })
   });
 
-  app.get('/auth', passport.authenticate('oidc'));
+  app.get('/auth', passport.authenticate("oauth2"));
   app.get('/auth/cb', wrapAuth);
 
   app.listen(port, () => console.log(`Example app listening on port ${port}!`));
@@ -281,9 +305,8 @@ const startApp = (client) => {
 
 (async () => {
   try {
-    const oidcClient = await createClient();
-    configurePassport(oidcClient);
-    startApp(oidcClient);
+    configurePassport();
+    startApp();
   } catch (err) {
     console.error(err);
     process.exit(1);
